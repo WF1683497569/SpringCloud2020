6e9703dbebcccd5a856edff32fe63a3b23b2476e
WF <1683497569@qq.com>
2020-03-24 22:08:27 +0800

    Stream

---
+++ springcloud-provider-stream-rabbitmq-8801/src/main/java/com/wf/springcloud/service/MessageProvider.java
@@ -0,0 +1,5 @@
+package com.wf.springcloud.service;
+
+public interface MessageProvider {
+    public String send();
+}
---
+++ springcloud-provider-stream-rabbitmq-8801/src/main/java/com/wf/springcloud/service/impl/MessageProviderImpl.java
@@ -0,0 +1,26 @@
+package com.wf.springcloud.service.impl;
+
+import com.wf.springcloud.service.MessageProvider;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+@EnableBinding(Source.class) //定义消息的推送管道
+@Slf4j
+public class MessageProviderImpl implements MessageProvider {
+    @Resource
+    private MessageChannel output; // 消息发送管道
+
+    @Override
+    public String send() {
+        String serial = UUID.randomUUID().toString();
+        output.send(MessageBuilder.withPayload(serial).build());
+        log.info("*****serial: "+serial);
+        return serial;
+    }
+}
